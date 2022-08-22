f4005aab377ecfcda51e736a5d4f0fd6c35135b7
JohnLewis9904 <90780139+JohnLewis9904@users.noreply.github.com>
2022-08-22 12:55:17 -0500

    Added basic functions to calculator

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.sql.SQLOutput;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,11 +22,104 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int sum = num1 + num2;
+        if(!number1ET.getText().toString().equals("") && !number2ET.getText().toString().equals("")){
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int sum = num1 + num2;
 
-        numberSumTV.setText("" + sum);
+            numberSumTV.setText("" + sum);
+        }else{
+            numberSumTV.setText("Please type a number in each box");
+        }
+
+    }
+
+    public void findDifference(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        if(!number1ET.getText().toString().equals("") && !number2ET.getText().toString().equals("")){
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int sum = num1 - num2;
+
+            numberSumTV.setText("" + sum);
+        }else{
+            numberSumTV.setText("Please type a number in each box");
+        }
+    }
+
+    public void findProduct(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        if(!number1ET.getText().toString().equals("") && !number2ET.getText().toString().equals("")){
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int sum = num1 * num2;
+
+            numberSumTV.setText("" + sum);
+        }else{
+            numberSumTV.setText("Please type a number in each box");
+        }
+    }
+
+    public void findQuotient(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        if(!number1ET.getText().toString().equals("") && !number2ET.getText().toString().equals("")){
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            double sum = num1 / num2;
+
+            numberSumTV.setText("" + sum);
+        }else{
+            numberSumTV.setText("Please type a number in each box");
+        }
+    }
+
+    public void findPow(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        if(!number1ET.getText().toString().equals("") && !number2ET.getText().toString().equals("")){
+            double num1 = Double.parseDouble((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            double sum = Math.pow(num1, num2);
+
+            numberSumTV.setText("" + sum);
+        }else{
+            numberSumTV.setText("Please type a number in each box");
+        }
+    }
+
+    public void findLog(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        if(!number1ET.getText().toString().equals("") && !number2ET.getText().toString().equals("")){
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            double sum = num1 / num2;
+
+            numberSumTV.setText("" + sum);
+        }else{
+            numberSumTV.setText("Please type a number in each box");
+        }
+    }
+
+    public void clear(View view){
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+
+        number1ET.setText("");
+        number2ET.setText("");
     }
 
 }
